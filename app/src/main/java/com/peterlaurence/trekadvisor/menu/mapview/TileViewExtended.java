a1431a65f590f43d96bfd039e5dee1daf89fb001
peterLaurence <peter.laurence@protonmail.com>
2017-02-19 18:54:17 +0100

    Add core functionality to draw path using hardware acceleration.

--- app/src/main/java/com/peterlaurence/trekadvisor/menu/mapview/TileViewExtended.java
+++ app/src/main/java/com/peterlaurence/trekadvisor/menu/mapview/TileViewExtended.java
@@ -1,11 +1,15 @@
 package com.peterlaurence.trekadvisor.menu.mapview;
 
 import android.content.Context;
+import android.graphics.Paint;
 import android.view.MotionEvent;
 
+import com.peterlaurence.trekadvisor.menu.mapview.components.PathView;
 import com.qozix.tileview.TileView;
+import com.qozix.tileview.geom.CoordinateTranslater;
 
 import java.lang.ref.WeakReference;
+import java.util.List;
 
 /**
  * Specialization of TileView to allow custom custom control of touch events.
@@ -18,9 +22,13 @@ public class TileViewExtended extends TileView {
     private boolean mScrollingAtStart;
 
     private WeakReference<ScrollListener> mScrollListenerWeakReference;
+    private PathView mPathView;
 
     public TileViewExtended(Context context) {
         super(context);
+
+        mPathView = new PathView(context);
+        addView(mPathView);
     }
 
     @Override
@@ -43,7 +51,7 @@ public class TileViewExtended extends TileView {
     }
 
     @Override
-    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1){
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
         if (mScrollListenerWeakReference != null) {
             ScrollListener scrollListener = mScrollListenerWeakReference.get();
             if (scrollListener != null) {
@@ -72,5 +80,40 @@ public class TileViewExtended extends TileView {
 
     public void setScrollListener(ScrollListener listener) {
         mScrollListenerWeakReference = new WeakReference<>(listener);
+    }
+
+    /**
+     * An alternative way to {@link #drawPath(List, Paint)}, which uses a {@link PathView}.
+     *
+     * @param positions {@link List} of coordinates, typically projected values
+     * @param paint     The Paint instance that defines the style of the drawn path.
+     * @return The {@link PathView.DrawablePath} instance passed to the TileView.
+     */
+    public PathView.DrawablePath drawPathQuickly(List<double[]> positions, Paint paint) {
+        CoordinateTranslater mCoordinateTranslater = getCoordinateTranslater();
+
+        int size;
+        if (positions.size() > 2) {
+            size = 4 * (positions.size() - 2) + 4;
+        } else {
+            size = 2 * positions.size();
+        }
+        float[] path = new float[size];
+        int i = 0;
+        for (double[] point : positions) {
+            if (i == 0) {
+                path[0] = (float) mCoordinateTranslater.translateX(point[0]);
+                path[1] = (float) mCoordinateTranslater.translateY(point[1]);
+                i += 2;
+            } else {
+                path[i] = (float) mCoordinateTranslater.translateX(point[0]);
+                path[i + 1] = (float) mCoordinateTranslater.translateY(point[1]);
+                if (i + 2 == path.length) break;
+                path[i + 2] = path[i];
+                path[i + 3] = path[i + 1];
+                i += 4;
+            }
+        }
+        return mPathView.addPath(path, paint);
     }
 }
---
+++ app/src/main/java/com/peterlaurence/trekadvisor/menu/mapview/components/PathView.java
@@ -0,0 +1,120 @@
+package com.peterlaurence.trekadvisor.menu.mapview.components;
+
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.View;
+
+import java.util.HashSet;
+
+/**
+ * This is a modified version of {@link com.qozix.tileview.paths.CompositePathView}, using
+ * {@code Canvas.drawLines} to draw a path. This method is much more efficient as it's
+ * hardware accelerated, although the result is not as neat as the original implementation (which
+ * uses {@link Path}.
+ *
+ * @author peterLaurence on 19/02/17
+ */
+public class PathView extends View {
+
+    private static final int DEFAULT_STROKE_COLOR = 0xFF000000;
+    private static final int DEFAULT_STROKE_WIDTH = 15;
+
+    private float mScale = 1;
+
+    private boolean mShouldDraw = true;
+
+    private HashSet<DrawablePath> mDrawablePaths = new HashSet<>();
+
+    private Paint mDefaultPaint = new Paint();
+
+    {
+        mDefaultPaint.setStyle(Paint.Style.STROKE);
+        mDefaultPaint.setColor(DEFAULT_STROKE_COLOR);
+        mDefaultPaint.setStrokeWidth(DEFAULT_STROKE_WIDTH);
+        mDefaultPaint.setAntiAlias(true);
+        mDefaultPaint.setStrokeJoin(Paint.Join.ROUND);
+        mDefaultPaint.setStrokeCap(Paint.Cap.ROUND);
+    }
+
+    public PathView(Context context) {
+        super(context);
+        setWillNotDraw(false);
+    }
+
+    public float getScale() {
+        return mScale;
+    }
+
+    public void setScale(float scale) {
+        mScale = scale;
+        invalidate();
+    }
+
+    public Paint getDefaultPaint() {
+        return mDefaultPaint;
+    }
+
+    public DrawablePath addPath(float[] path, Paint paint) {
+        if (paint == null) {
+            paint = mDefaultPaint;
+        }
+        DrawablePath DrawablePath = new DrawablePath();
+        DrawablePath.path = path;
+        DrawablePath.paint = paint;
+        return addPath(DrawablePath);
+    }
+
+    public DrawablePath addPath(DrawablePath DrawablePath) {
+        mDrawablePaths.add(DrawablePath);
+        invalidate();
+        return DrawablePath;
+    }
+
+    public void removePath(DrawablePath path) {
+        mDrawablePaths.remove(path);
+        invalidate();
+    }
+
+    public void clear() {
+        mDrawablePaths.clear();
+        invalidate();
+    }
+
+    public void setShouldDraw(boolean shouldDraw) {
+        mShouldDraw = shouldDraw;
+        invalidate();
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        if (mShouldDraw) {
+            for (DrawablePath drawablePath : mDrawablePaths) {
+                canvas.scale(mScale, mScale);
+                drawablePath.paint.setStrokeWidth(drawablePath.width / mScale);
+                canvas.drawLines(drawablePath.path, drawablePath.paint);
+            }
+        }
+        super.onDraw(canvas);
+    }
+
+    public static class DrawablePath {
+
+        /**
+         * The path that this drawable will follow.
+         */
+        public float[] path;
+
+        /**
+         * The paint to be used for this path.
+         */
+        public Paint paint;
+
+        /**
+         * The width of the path
+         */
+        public float width = DEFAULT_STROKE_WIDTH;
+    }
+}
