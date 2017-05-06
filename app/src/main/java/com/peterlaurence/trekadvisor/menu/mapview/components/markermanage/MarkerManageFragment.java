2ff5ee53a1feb19a668d404215285500db369c7f
peterLaurence <peter.laurence@protonmail.com>
2017-05-06 09:56:22 +0200

    Fill marker fields.

--- app/src/main/java/com/peterlaurence/trekadvisor/menu/mapview/components/markermanage/MarkerManageFragment.java
+++ app/src/main/java/com/peterlaurence/trekadvisor/menu/mapview/components/markermanage/MarkerManageFragment.java
@@ -3,17 +3,21 @@ package com.peterlaurence.trekadvisor.menu.mapview.components.markermanage;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.peterlaurence.trekadvisor.R;
 import com.peterlaurence.trekadvisor.core.map.Map;
 import com.peterlaurence.trekadvisor.core.map.gson.MarkerGson;
 import com.peterlaurence.trekadvisor.menu.MapProvider;
 import com.peterlaurence.trekadvisor.menu.MarkerProvider;
+
+import static android.view.View.GONE;
 
 /**
  * A {@link Fragment} subclass that provides tools to :
@@ -33,6 +37,13 @@ public class MarkerManageFragment extends Fragment {
 
     private Map mMap;
     private MarkerGson.Marker mMarker;
+
+    private TextInputEditText mNameEditText;
+    private TextInputEditText mLatEditText;
+    private TextInputEditText mLonEditText;
+    private TextView mProjectionLabel;
+    private TextInputEditText mProjectionX;
+    private TextInputEditText mProjectionY;
 
     @Override
     public void onAttach(Context context) {
@@ -57,8 +68,19 @@ public class MarkerManageFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         rootView = inflater.inflate(R.layout.fragment_marker_manage, container, false);
+
+        /* The view fields */
+        mNameEditText = (TextInputEditText) rootView.findViewById(R.id.marker_name_id);
+        mLatEditText = (TextInputEditText) rootView.findViewById(R.id.marker_lat_id);
+        mLonEditText = (TextInputEditText) rootView.findViewById(R.id.marker_lon_id);
+        mProjectionLabel = (TextView) rootView.findViewById(R.id.marker_proj_label_id);
+        mProjectionX = (TextInputEditText) rootView.findViewById(R.id.marker_proj_x_id);
+        mProjectionY = (TextInputEditText) rootView.findViewById(R.id.marker_proj_y_id);
+
         mMap = mMapProvider.getCurrentMap();
         mMarker = mMarkerProvider.getCurrentMarker();
+
+        updateView();
         return rootView;
     }
 
@@ -66,5 +88,22 @@ public class MarkerManageFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         menu.clear();
         super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private void updateView() {
+        mNameEditText.setText(mMarker.name);
+        mLatEditText.setText(String.valueOf(mMarker.lat));
+        mLonEditText.setText(String.valueOf(mMarker.lon));
+
+        /* Check whether projected coordinates fields should be shown or not */
+        if (mMap.getProjection() == null) {
+            mProjectionLabel.setVisibility(GONE);
+            mProjectionX.setVisibility(GONE);
+            mProjectionY.setVisibility(GONE);
+            return;
+        }
+
+        mProjectionX.setText(String.valueOf(mMarker.proj_x));
+        mProjectionY.setText(String.valueOf(mMarker.proj_y));
     }
 }
