40a9f4975c774f67603537f454c83f07b4279a23
peterLaurence <peter.laurence@protonmail.com>
2017-12-29 00:34:30 +0100

    Use Animated Vector Drawables for the button that controls the location service.

--- app/src/main/java/com/peterlaurence/trekadvisor/menu/record/components/ActionsView.java
+++ app/src/main/java/com/peterlaurence/trekadvisor/menu/record/components/ActionsView.java
@@ -3,12 +3,12 @@ package com.peterlaurence.trekadvisor.menu.record.components;
 import android.content.Context;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
-import android.widget.ImageButton;
 
 import com.peterlaurence.trekadvisor.R;
 import com.peterlaurence.trekadvisor.menu.events.RecordGpxStartEvent;
 import com.peterlaurence.trekadvisor.menu.record.components.events.RequestStartEvent;
 import com.peterlaurence.trekadvisor.menu.record.components.events.RequestStopEvent;
+import com.peterlaurence.trekadvisor.menu.tools.DelayedButton;
 import com.peterlaurence.trekadvisor.service.event.LocationServiceStatus;
 
 import org.greenrobot.eventbus.EventBus;
@@ -20,8 +20,7 @@ import org.greenrobot.eventbus.Subscribe;
  * @author peterLaurence on 23/12/17.
  */
 public class ActionsView extends CardView {
-    private ImageButton mButton;
-    private State mState = State.WaitingStatus;
+    private DelayedButton mButton;
 
     public ActionsView(Context context) {
         this(context, null);
@@ -40,8 +39,18 @@ public class ActionsView extends CardView {
     private void init(Context context, AttributeSet attrs) {
         inflate(context, R.layout.record_actions_card, this);
 
-        mButton = getRootView().findViewById(R.id.recordControlButton);
-        mButton.setOnClickListener(v -> toggleRecord());
+        mButton = getRootView().findViewById(R.id.delayedButton);
+        mButton.setListener(new DelayedButton.PlayStopListener() {
+            @Override
+            public void onPlay() {
+                requestStart();
+            }
+
+            @Override
+            public void onStop() {
+                requestStop();
+            }
+        });
 
         initState();
     }
@@ -54,45 +63,9 @@ public class ActionsView extends CardView {
     private void initState() {
 
         if (EventBus.getDefault().hasSubscriberForEvent(RecordGpxStartEvent.class)) {
-            mState = State.ServiceStarted;
+            mButton.setMode(DelayedButton.State.STOP);
         } else {
-            mState = State.ServiceStopped;
-        }
-
-        processState();
-    }
-
-    private void processState() {
-        switch (mState) {
-            case ServiceStarted:
-                mButton.setImageResource(R.drawable.ic_stop_black_24dp);
-                mButton.setEnabled(true);
-                break;
-            case ServiceStopped:
-                mButton.setImageResource(R.drawable.ic_play_arrow_black_24dp);
-                mButton.setEnabled(true);
-                break;
-            case WaitingStatus:
-                mButton.setEnabled(false);
-                break;
-            default:
-                // don't care
-        }
-    }
-
-    private void toggleRecord() {
-        switch (mState) {
-            case ServiceStarted:
-                requestStop();
-                break;
-            case ServiceStopped:
-                requestStart();
-                break;
-            case WaitingStatus:
-                /* This should never happen */
-                processState();
-            default:
-                // don't care
+            mButton.setMode(DelayedButton.State.PLAY);
         }
     }
 
@@ -102,10 +75,6 @@ public class ActionsView extends CardView {
      */
     private void requestStop() {
         EventBus.getDefault().post(new RequestStopEvent());
-
-        /* Then we assume the service is stopped */
-        mState = State.ServiceStopped;
-        processState();
     }
 
     /**
@@ -114,19 +83,10 @@ public class ActionsView extends CardView {
      */
     private void requestStart() {
         EventBus.getDefault().post(new RequestStartEvent());
-
-        /* Then wait for the start event confirmation */
-        mState = State.WaitingStatus;
-        processState();
     }
 
     @Subscribe
     public void onLocationServiceStatus(LocationServiceStatus event) {
-        mState = event.started ? State.ServiceStarted : State.ServiceStopped;
-        processState();
-    }
-
-    private enum State {
-        ServiceStarted, ServiceStopped, WaitingStatus
+        // TODO : take into account some errors coming back from the service
     }
 }
---
+++ app/src/main/java/com/peterlaurence/trekadvisor/menu/tools/DelayedButton.java
@@ -0,0 +1,120 @@
+package com.peterlaurence.trekadvisor.menu.tools;
+
+import android.content.Context;
+import android.graphics.drawable.Animatable2;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.AppCompatImageButton;
+import android.util.AttributeSet;
+
+import com.peterlaurence.trekadvisor.R;
+
+/**
+ * A button which has two modes : Play and Stop. <br>
+ * There is a cooldown between each mode switch, to avoid a quick change of state. <br>
+ * This is implemented using Animated Vector Drawables, so the cooldown is defined in the animations.
+ *
+ * @author peterLaurence on 26/12/17.
+ */
+public class DelayedButton extends AppCompatImageButton {
+    private AnimatedVectorDrawable mStopToPLayDrawable;
+    private AnimatedVectorDrawable mPlayToStopDrawable;
+    private State mState;
+    private PlayStopListener mListener;
+
+    public DelayedButton(Context context) {
+        this(context, null);
+    }
+
+    public DelayedButton(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DelayedButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        mStopToPLayDrawable = (AnimatedVectorDrawable) context.getDrawable(R.drawable.avd_delayed_button_stop_to_play);
+        mPlayToStopDrawable = (AnimatedVectorDrawable) context.getDrawable(R.drawable.avd_delayed_button_play_to_stop);
+        setImageDrawable(mPlayToStopDrawable);
+
+        mState = State.PLAY;
+
+        mStopToPLayDrawable.registerAnimationCallback(new Animatable2.AnimationCallback() {
+            @Override
+            public void onAnimationEnd(Drawable drawable) {
+                super.onAnimationEnd(drawable);
+
+                setEnabled(true);
+            }
+        });
+
+        mPlayToStopDrawable.registerAnimationCallback(new Animatable2.AnimationCallback() {
+            @Override
+            public void onAnimationEnd(Drawable drawable) {
+                super.onAnimationEnd(drawable);
+
+                setEnabled(true);
+            }
+        });
+
+        setOnClickListener(v -> toggle());
+    }
+
+    public void setListener(PlayStopListener listener) {
+        mListener = listener;
+    }
+
+    public void setMode(State state) {
+        switch (state) {
+            case STOP:
+                setImageDrawable(mStopToPLayDrawable);
+                mState = State.STOP;
+                break;
+            case PLAY:
+                setImageDrawable(mPlayToStopDrawable);
+                mState = State.PLAY;
+                break;
+            default:
+                // don't care
+        }
+    }
+
+    public void toggle() {
+        switch (mState) {
+            case STOP:
+                setEnabled(false);
+                setImageDrawable(mStopToPLayDrawable);
+                mStopToPLayDrawable.start();
+                mState = State.PLAY;
+
+                if (mListener != null) {
+                    mListener.onStop();
+                }
+                break;
+
+            case PLAY:
+                setEnabled(false);
+                setImageDrawable(mPlayToStopDrawable);
+                mPlayToStopDrawable.start();
+                mState = State.STOP;
+
+                if (mListener != null) {
+                    mListener.onPlay();
+                }
+                break;
+
+            default:
+                // don't care
+        }
+    }
+
+    public enum State {
+        STOP, PLAY
+    }
+
+    public interface PlayStopListener {
+        void onPlay();
+
+        void onStop();
+    }
+}
