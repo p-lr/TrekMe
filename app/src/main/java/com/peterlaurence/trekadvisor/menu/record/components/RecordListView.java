13e1b43960a6c327ff6afc4794d88bbb68aafa76
peterLaurence <peter.laurence@protonmail.com>
2018-02-15 20:39:12 +0100

    Implement recording deletion.

--- app/src/main/java/com/peterlaurence/trekadvisor/menu/record/components/RecordListView.java
+++ app/src/main/java/com/peterlaurence/trekadvisor/menu/record/components/RecordListView.java
@@ -1,11 +1,13 @@
 package com.peterlaurence.trekadvisor.menu.record.components;
 
 import android.content.Context;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.ImageButton;
 
 import com.peterlaurence.trekadvisor.R;
 import com.peterlaurence.trekadvisor.core.track.TrackImporter;
@@ -24,6 +26,7 @@ public class RecordListView extends CardView {
     private boolean mIsMultiSelectMode = false;
     private ArrayList<File> mSelectedRecordings = new ArrayList<>();
     private ArrayList<File> mRecordings;
+    private RecordingAdapter mRecordingAdapter;
 
     public RecordListView(Context context) {
         this(context, null);
@@ -45,11 +48,34 @@ public class RecordListView extends CardView {
 
         Context ctx = getContext();
         RecyclerView recyclerView = findViewById(R.id.recordings_recycler_id);
+        ImageButton deleteRecordingButton = findViewById(R.id.delete_recording_button);
+
+        deleteRecordingButton.setOnClickListener(v -> {
+            boolean success = true;
+            for (File file : mSelectedRecordings) {
+                if (file.exists()) {
+                    if (file.delete()) {
+                        mRecordings.remove(file);
+                    } else {
+                        success = false;
+                    }
+                }
+            }
+            mRecordingAdapter.notifyDataSetChanged();
+
+            /* Alert the user that some files could not be deleted */
+            if (!success) {
+                Snackbar snackbar = Snackbar.make(getRootView(), R.string.files_could_not_be_deleted,
+                        Snackbar.LENGTH_SHORT);
+                snackbar.show();
+            }
+        });
+
         LinearLayoutManager llm = new LinearLayoutManager(ctx);
         recyclerView.setLayoutManager(llm);
 
-        RecordingAdapter recordingAdapter = new RecordingAdapter(mRecordings, mSelectedRecordings);
-        recyclerView.setAdapter(recordingAdapter);
+        mRecordingAdapter = new RecordingAdapter(mRecordings, mSelectedRecordings);
+        recyclerView.setAdapter(mRecordingAdapter);
 
         recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this.getContext(),
                 recyclerView, new RecyclerItemClickListener.OnItemClickListener() {
@@ -58,8 +84,8 @@ public class RecordListView extends CardView {
                 if (mIsMultiSelectMode) {
                     multiSelect(position);
 
-                    recordingAdapter.setSelectedRecordings(mSelectedRecordings);
-                    recordingAdapter.notifyItemChanged(position);
+                    mRecordingAdapter.setSelectedRecordings(mSelectedRecordings);
+                    mRecordingAdapter.notifyItemChanged(position);
                 }
             }
 
@@ -69,12 +95,12 @@ public class RecordListView extends CardView {
                 if (!mIsMultiSelectMode) {
                     mIsMultiSelectMode = true;
                     multiSelect(position);
-                    recordingAdapter.setSelectedRecordings(mSelectedRecordings);
-                    recordingAdapter.notifyItemChanged(position);
+                    mRecordingAdapter.setSelectedRecordings(mSelectedRecordings);
+                    mRecordingAdapter.notifyItemChanged(position);
                 } else {
                     mIsMultiSelectMode = false;
-                    recordingAdapter.setSelectedRecordings(mSelectedRecordings);
-                    recordingAdapter.notifyDataSetChanged();
+                    mRecordingAdapter.setSelectedRecordings(mSelectedRecordings);
+                    mRecordingAdapter.notifyDataSetChanged();
                 }
             }
         }));
