04d3c9df09414955d61ec099b5b1df1a9c412e53
peterLaurence <peter.laurence@protonmail.com>
2019-06-08 09:11:49 +0200

    Change TileCollector design

--- app/src/main/java/com/peterlaurence/mapview/core/TileCollector.kt
+++ app/src/main/java/com/peterlaurence/mapview/core/TileCollector.kt
@@ -10,14 +10,18 @@ import kotlinx.coroutines.selects.select
 const val N_WORKERS = 4
 
 /**
- * @param [visibleTiles] channel of [VisibleTiles], which capacity should be [Channel.CONFLATED].
+ * @param [visibleTileLocations] channel of [TileLocation], which capacity should be [Channel.CONFLATED].
+ * @param [tilesOutput] channel of [Tile], which capacity should be [Channel.UNLIMITED]
  */
-fun CoroutineScope.processVisibleTiles(visibleTiles: ReceiveChannel<VisibleTiles>) {
+fun CoroutineScope.processVisibleTiles(visibleTileLocations: ReceiveChannel<List<TileLocation>>,
+                                       tilesOutput: SendChannel<Tile>,
+                                       tileProvider: TileProvider) {
     val tilesToDownload = Channel<Tile>(capacity = Channel.UNLIMITED)
-    val tilesDownloaded = Channel<Tile>(capacity = Channel.UNLIMITED)
+    val tilesDownloadedFromWorker = Channel<Tile>(capacity = Channel.UNLIMITED)
 
-    repeat(N_WORKERS) { worker(tilesToDownload, tilesDownloaded) }
-    tileCollector(visibleTiles, tilesToDownload, tilesDownloaded)
+    repeat(N_WORKERS) { worker(tilesToDownload, tilesDownloadedFromWorker) }
+    tileCollector(visibleTileLocations, tilesToDownload, tilesDownloadedFromWorker, tilesOutput,
+            tileProvider)
 }
 
 fun CoroutineScope.worker(tilesToDownload: ReceiveChannel<Tile>,
@@ -28,58 +32,38 @@ fun CoroutineScope.worker(tilesToDownload: ReceiveChannel<Tile>,
     }
 }
 
-fun CoroutineScope.tileCollector(visibleTiles: ReceiveChannel<VisibleTiles>,
+fun CoroutineScope.tileCollector(tileLocations: ReceiveChannel<List<TileLocation>>,
                                  tilesToDownload: SendChannel<Tile>,
-                                 tilesDownloaded: ReceiveChannel<Tile>) = launch {
+                                 tilesDownloadedFromWorker: ReceiveChannel<Tile>,
+                                 tilesOutput: SendChannel<Tile>,
+                                 tileProvider: TileProvider) = launch {
 
-    var lastVisibleTiles = VisibleTiles(-1, 0, 0, 0, 0)
-    val tilesCollected = mutableListOf<Tile>()
+    val locationsBeingProcessed = mutableListOf<TileLocation>()
 
     while (true) {
         select<Unit> {
-            visibleTiles.onReceive {
-                dispatchTiles(it, tilesToDownload)
+            tileLocations.onReceive {
+                for (loc in it) {
+                    if (!locationsBeingProcessed.contains(loc)) {
+                        /* Add it to the list of locations being processed */
+                        locationsBeingProcessed.add(loc)
 
-                /* Only keep tiles that are visible */
-                if (lastVisibleTiles != it) {
-                    tilesCollected.keepOnlyThoseInside(it)
+                        /* Now download the tile */
+                        val tile = tileProvider.getTile(loc.zoom, loc.row, loc.col)
+                        tilesToDownload.send(tile)
+                    }
                 }
-
-                lastVisibleTiles = it
             }
 
-            tilesDownloaded.onReceive {
-                if (it.isInside(lastVisibleTiles)) {
-                    tilesCollected.add(it)
-                }
+            tilesDownloadedFromWorker.onReceive {
+                tilesOutput.send(it)
+
+                /* Now remove the location from the list */
+                val loc = TileLocation(it.zoom, it.row, it.col)
+                locationsBeingProcessed.remove(loc)
             }
         }
     }
-}
-
-/**
- * If a tile is already stored inside the LRU cache, get it from there.
- */
-private suspend fun dispatchTiles(visibleTiles: VisibleTiles,
-                                  tilesToDownload: SendChannel<Tile>): List<Tile> {
-    for (location in visibleTiles.toTileLocations()) {
-        // if is already inside LRU cache, get it from there
-        // else, send them to the tilesToDownload channel
-    }
-    TODO()
-}
-
-private fun VisibleTiles.toTileLocations(): List<TileLocation> {
-    // simply convert to a tile list
-    TODO()
-}
-
-private fun Tile.isInside(visibleTiles: VisibleTiles): Boolean {
-    TODO()
-}
-
-private fun MutableList<Tile>.keepOnlyThoseInside(visibleTiles: VisibleTiles): List<Tile> = apply {
-    TODO()
 }
 
 data class TileLocation(val zoom: Int, val row: Int, val col: Int)
