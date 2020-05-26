128b1e9036739302692713164d24765fe017a22c
plr <plr.devs@gmail.com>
2020-05-26 19:17:32 +0200

    Avoid memory leak in RecordFragment

--- app/src/main/java/com/peterlaurence/trekme/ui/record/RecordFragment.kt
+++ app/src/main/java/com/peterlaurence/trekme/ui/record/RecordFragment.kt
@@ -53,6 +53,11 @@ class RecordFragment : Fragment() {
         return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
