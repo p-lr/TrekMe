54a36cd35f4ce220727e851da4fa2192f0ebab82
plr <plr.devs@gmail.com>
2020-07-31 11:26:26 +0200

    Change layer to Outdoor

--- app/src/main/java/com/peterlaurence/trekme/core/providers/urltilebuilder/UrlTileBuilderOrdnanceSurvey.kt
+++ app/src/main/java/com/peterlaurence/trekme/core/providers/urltilebuilder/UrlTileBuilderOrdnanceSurvey.kt
@@ -2,6 +2,6 @@ package com.peterlaurence.trekme.core.providers.urltilebuilder
 
 class UrlTileBuilderOrdnanceSurvey(private val api: String) : UrlTileBuilder {
     override fun build(level: Int, row: Int, col: Int): String {
-        return "https://api.os.uk/maps/raster/v1/wmts?key=$api&service=WMTS&version=1.0.0&request=GetTile&layer=Light_3857&style=Light&format=image/png&tileMatrixSet=EPSG:3857&tileMatrix=$level&tileRow=$row&tileCol=$col"
+        return "https://api.os.uk/maps/raster/v1/wmts?key=$api&service=WMTS&version=1.0.0&request=GetTile&layer=Outdoor_3857&style=Outdoor&format=image/png&tileMatrixSet=EPSG:3857&tileMatrix=$level&tileRow=$row&tileCol=$col"
     }
 }
