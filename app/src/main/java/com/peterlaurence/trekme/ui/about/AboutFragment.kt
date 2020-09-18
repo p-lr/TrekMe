63a986d5424e4efe26ba9cc67ea59a8ac5a5a93d
plr <plr.devs@gmail.com>
2020-09-18 15:55:05 +0200

    Encourage feedback

--- app/src/main/java/com/peterlaurence/trekme/ui/about/AboutFragment.kt
+++ app/src/main/java/com/peterlaurence/trekme/ui/about/AboutFragment.kt
@@ -1,11 +1,13 @@
 package com.peterlaurence.trekme.ui.about
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.peterlaurence.trekme.R
 import com.peterlaurence.trekme.databinding.FragmentAboutBinding
@@ -21,7 +23,7 @@ class AboutFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                               savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View? {
         _binding = FragmentAboutBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -33,6 +35,34 @@ class AboutFragment : Fragment() {
             val url = getString(R.string.help_url)
             val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             startActivity(browserIntent)
+        }
+
+        /* In case of positive feedback, ask for app rating */
+        binding.thumbUpButton.setOnClickListener {
+            val packageName = requireContext().applicationContext.packageName
+            val uri: Uri = Uri.parse("market://details?id=$packageName")
+            val goToMarket = Intent(Intent.ACTION_VIEW, uri)
+            try {
+                startActivity(goToMarket)
+            } catch (e: ActivityNotFoundException) {
+                startActivity(Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://play.google.com/store/apps/details?id=$packageName")))
+            }
+        }
+
+        /* In case of negative feedback, ask the user to send an email */
+        binding.thumbDownButton.setOnClickListener {
+            AlertDialog.Builder(requireContext())
+                    .setMessage(getString(R.string.feedback_negative_msg))
+                    .setPositiveButton(getString(R.string.ok_dialog)) { _, _ ->
+                        val emailIntent = Intent(Intent.ACTION_SENDTO,
+                                Uri.fromParts("mailto", getString(R.string.email_support), null))
+                        startActivity(emailIntent)
+                    }
+                    .setNegativeButton(getString(R.string.cancel_dialog_string)) { d, _ ->
+                        d.cancel()
+                    }
+                    .create().show()
         }
     }
 
