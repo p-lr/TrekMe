c732c18c4c0cb18202fba05bb30bac41c9fcda8b
plr <plr.devs@gmail.com>
2021-04-12 19:03:35 +0200

    Fix OSM map

--- app/src/main/java/com/peterlaurence/trekme/core/providers/stream/TileStreamProviderOSM.kt
+++ app/src/main/java/com/peterlaurence/trekme/core/providers/stream/TileStreamProviderOSM.kt
@@ -5,6 +5,7 @@ import com.peterlaurence.trekme.core.map.TileStreamProvider
 import com.peterlaurence.trekme.core.providers.bitmap.TileStreamProviderHttp
 import com.peterlaurence.trekme.core.providers.bitmap.TileStreamProviderRetry
 import com.peterlaurence.trekme.core.providers.urltilebuilder.UrlTileBuilder
+import kotlin.random.Random
 
 /**
  * A specific [TileStreamProvider] for OpenStreetMap.
@@ -13,11 +14,24 @@ import com.peterlaurence.trekme.core.providers.urltilebuilder.UrlTileBuilder
  */
 class TileStreamProviderOSM(urlTileBuilder: UrlTileBuilder) : TileStreamProvider {
     private val requestProperties = mapOf(
-            "User-Agent" to "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
+            "User-Agent" to generateRandomUserAgent()
     )
     private val base = TileStreamProviderRetry(TileStreamProviderHttp(urlTileBuilder, requestProperties))
+
+    private fun generateRandomUserAgent(): String {
+        val length = Random.nextInt(5, 25)
+        return (0..length)
+                .map { Random.nextInt(0, charPool.size) }
+                .map(charPool::get)
+                .joinToString("")
+    }
 
     override fun getTileStream(row: Int, col: Int, zoomLvl: Int): TileResult {
         return base.getTileStream(row, col, zoomLvl)
     }
 }
+
+private val charPool = arrayOf('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6',
+        '7', '8', '9'
+)
