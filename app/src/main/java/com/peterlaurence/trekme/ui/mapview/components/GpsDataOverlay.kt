8628bf7236161ffd5833b71442980a6c1e161bb9
plr <plr.devs@gmail.com>
2021-07-17 17:46:49 +0200

    Show the name of external GPS

--- app/src/main/java/com/peterlaurence/trekme/ui/mapview/components/GpsDataOverlay.kt
+++ app/src/main/java/com/peterlaurence/trekme/ui/mapview/components/GpsDataOverlay.kt
@@ -15,10 +15,12 @@ import androidx.compose.ui.platform.AbstractComposeView
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.peterlaurence.trekme.R
 import com.peterlaurence.trekme.core.model.Location
+import com.peterlaurence.trekme.core.model.LocationProducerBtInfo
 import com.peterlaurence.trekme.core.units.UnitFormatter
 import com.peterlaurence.trekme.ui.theme.TrekMeTheme
 
@@ -26,29 +28,37 @@ import com.peterlaurence.trekme.ui.theme.TrekMeTheme
 @Composable
 fun GpsDataOverlay(location: Location?) {
     Column(Modifier
+            .width(IntrinsicSize.Min)
             .background(colorResource(id = R.color.colorIndicatorOverlay), RoundedCornerShape(topEnd = 5.dp))
             .padding(8.dp)
     ) {
+        /* When an external source is used, display the name of the device */
+        when (val p = location?.locationProducerInfo) {
+            is LocationProducerBtInfo -> Text(
+                    p.name,
+                    Modifier.fillMaxWidth(), maxLines = 1, overflow = TextOverflow.Ellipsis,
+                    color = colorResource(id = R.color.colorPrimaryTextWhite)
+            )
+        }
         KeyValueRow(key = stringResource(id = R.string.latitude_short),
-                value = location?.latitude?.let { UnitFormatter.formatLatLon(location.latitude) } ?: "")
+                value = location?.latitude?.let { UnitFormatter.formatLatLon(location.latitude) }
+                        ?: "")
         KeyValueRow(key = stringResource(id = R.string.longitude_short),
-                value = location?.longitude?.let { UnitFormatter.formatLatLon(location.longitude) } ?: "")
+                value = location?.longitude?.let { UnitFormatter.formatLatLon(location.longitude) }
+                        ?: "")
         KeyValueRow(key = stringResource(id = R.string.elevation_short),
-                value = location?.altitude?.let { UnitFormatter.formatElevation(location.altitude) } ?: "")
+                value = location?.altitude?.let { UnitFormatter.formatElevation(location.altitude) }
+                        ?: "")
     }
 }
 
 @Composable
 private fun KeyValueRow(key: String, value: String) {
+    val color = colorResource(id = R.color.colorPrimaryTextWhite)
     Row {
-        Text(key, color = colorResource(id = R.color.colorPrimaryTextWhite),
-                fontSize = 14.sp,
-                fontFamily = FontFamily.Monospace)
+        Text(key, color = color, fontSize = 14.sp, fontFamily = FontFamily.Monospace)
         Spacer(modifier = Modifier.width(8.dp))
-        Text(value, color = colorResource(id = R.color.colorPrimaryTextWhite),
-                fontSize = 14.sp,
-                fontFamily = FontFamily.Monospace)
-
+        Text(value, color = color, fontSize = 14.sp, fontFamily = FontFamily.Monospace)
     }
 }
 
