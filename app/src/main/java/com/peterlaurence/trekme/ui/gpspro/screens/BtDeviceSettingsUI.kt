08dfc59c1834035324881eb1c6280ba60025ee16
plr <plr.devs@gmail.com>
2021-07-24 11:43:16 +0200

    Add a button and an AlertDialog - wip

--- app/src/main/java/com/peterlaurence/trekme/ui/gpspro/screens/BtDeviceSettingsUI.kt
+++ app/src/main/java/com/peterlaurence/trekme/ui/gpspro/screens/BtDeviceSettingsUI.kt
@@ -2,17 +2,19 @@ package com.peterlaurence.trekme.ui.gpspro.screens
 
 import android.content.Context
 import android.util.AttributeSet
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.Divider
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.AbstractComposeView
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -29,14 +31,69 @@ import com.peterlaurence.trekme.viewmodel.gpspro.PairedDeviceList
 fun BtDeviceSettingsUI(btDeviceStub: BluetoothDeviceStub) {
     val color = Color(0xFF4CAF50)
     Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-        Spacer(Modifier.height(24.dp))
-        IconCircle(backgroundColor = color, 50.dp, R.drawable.bluetooth)
-        Spacer(Modifier.height(8.dp))
-        Text(btDeviceStub.name, fontSize = 18.sp, fontWeight = FontWeight.Medium)
-        Spacer(Modifier.height(16.dp))
-        Divider()
+        Header(name = btDeviceStub.name, color)
+        RecordButton()
     }
 
+}
+
+@Composable
+private fun Header(name: String, color: Color) {
+    Spacer(Modifier.height(24.dp))
+    IconCircle(backgroundColor = color, 50.dp, R.drawable.bluetooth)
+    Spacer(Modifier.height(8.dp))
+    Text(name, fontSize = 18.sp, fontWeight = FontWeight.Medium)
+    Spacer(Modifier.height(16.dp))
+    Divider()
+}
+
+@Composable
+private fun RecordButton() {
+    val openDialog = remember { mutableStateOf(false) }
+    Row(Modifier
+            .fillMaxWidth()
+            .clickable {
+                openDialog.value = true
+            }
+    ) {
+        Text(
+                text = stringResource(id = R.string.bt_device_frgmt_record),
+                modifier = Modifier.padding(start = 74.dp, top = 24.dp, bottom = 24.dp),
+        )
+        if (openDialog.value) {
+            ShowDialog(openDialog)
+        }
+    }
+
+    Divider()
+}
+
+@Composable
+private fun ShowDialog(openDialog: MutableState<Boolean>) {
+    AlertDialog(
+            onDismissRequest = { openDialog.value = false },
+            title = { Text(text = stringResource(id = R.string.bt_device_frgmt_diag_title)) },
+            text = {
+                Text("Here is a text ")
+            },
+            confirmButton = {
+                OutlinedButton(
+                        colors = ButtonDefaults.outlinedButtonColors(contentColor =  colorResource(id = R.color.colorGreen)),
+                        onClick = {
+                            openDialog.value = false
+                        }) {
+                    Text(stringResource(id = R.string.start_dialog_string))
+                }
+            },
+            dismissButton = {
+                OutlinedButton(
+                        onClick = {
+                            openDialog.value = false
+                        }) {
+                    Text(stringResource(id = R.string.cancel_dialog_string))
+                }
+            }
+    )
 }
 
 class BtDeviceSettingsUI @JvmOverloads constructor(
