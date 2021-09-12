a0e0f397bd6c767276ccf427bd7fe2fc22f22c3b
plr <plr.devs@gmail.com>
2021-09-12 14:08:28 +0200

    Add unit test for new route format

--- app/src/test/java/com/peterlaurence/trekme/core/map/MapParserTest.kt
+++ app/src/test/java/com/peterlaurence/trekme/core/map/MapParserTest.kt
@@ -23,50 +23,95 @@ class MapParserTest {
     private val routeRepository = RouteRepository(Dispatchers.IO, Dispatchers.Unconfined)
 
     @Test
-    fun mapTracksParse() = runBlocking {
-        if (jsonFilesDirectory != null) {
-            val dirs = listOf(jsonFilesDirectory)
-            mapLoader.clearMaps()
-            val mapList = mapLoader.updateMaps(dirs.filterNotNull())
+    fun legacyMapRoutesParse() = runBlocking {
+        val mapDirURL =
+            MapImporterTest::class.java.classLoader!!.getResource("map-with-legacy-routes")
+        val mapDir = File(mapDirURL.toURI())
 
-            /* One map should be found */
-            assertEquals(1, mapList.size.toLong())
-            val map = mapList[0]
+        assertTrue(mapDir.exists())
 
-            routeRepository.importRoutes(map)
-            assertEquals(2, map.routes!!.size.toLong())
+        val dirs = listOf(mapDir)
+        mapLoader.clearMaps()
+        val mapList = mapLoader.updateMaps(dirs.filterNotNull())
 
-            val route = map.routes!![0]
-            assertEquals("A test route 1", route.name)
-            assertTrue(route.visible)
-            val markers = route.routeMarkers
-            assertEquals(2, markers.size.toLong())
+        /* One map should be found */
+        assertEquals(1, mapList.size.toLong())
+        val map = mapList[0]
 
-            val marker1 = markers[0]
-            assertEquals("marker1", marker1.name)
-            val projX = marker1.proj_x
-            assertNotNull(projX)
-            assertEquals(6198798.5047565, projX, 0.0)
+        routeRepository.importRoutes(map)
+        assertEquals(2, map.routes!!.size.toLong())
 
-            val marker2 = markers[1]
-            assertEquals("marker2", marker2.name)
-            val projY = marker2.proj_y
-            assertNotNull(projY)
-            assertEquals(-2418744.7142449305, projY, 0.0)
-        }
+        val route = map.routes!![0]
+        assertEquals("A test route 1", route.name)
+        assertTrue(route.visible)
+        val markers = route.routeMarkers
+        assertEquals(2, markers.size.toLong())
+
+        val marker1 = markers[0]
+        assertEquals("marker1", marker1.name)
+        val projX = marker1.proj_x
+        assertNotNull(projX)
+        assertEquals(6198798.5047565, projX, 0.0)
+
+        val marker2 = markers[1]
+        assertEquals("marker2", marker2.name)
+        val projY = marker2.proj_y
+        assertNotNull(projY)
+        assertEquals(-2418744.7142449305, projY, 0.0)
     }
 
-    companion object {
-        private var jsonFilesDirectory: File? = null
+    @Test
+    fun mapRoutesParse() = runBlocking {
+        val mapDirURL = MapImporterTest::class.java.classLoader!!.getResource("map-with-routes")
+        val mapDir = File(mapDirURL.toURI())
 
-        init {
-            try {
-                val mapDirURL = MapImporterTest::class.java.classLoader!!.getResource("mapjson-example")
-                jsonFilesDirectory = File(mapDirURL.toURI())
-            } catch (e: Exception) {
-                println("No json directory found.")
-            }
+        assertTrue(mapDir.exists())
 
-        }
+        val dirs = listOf(mapDir)
+        mapLoader.clearMaps()
+        val mapList = mapLoader.updateMaps(dirs)
+
+        /* One map should be found */
+        assertEquals(1, mapList.size.toLong())
+        val map = mapList[0]
+
+        routeRepository.importRoutes(map)
+        assertEquals(2, map.routes!!.size.toLong())
+
+        val route1 = map.routes!!.sortedBy { it.name }[0]
+        assertEquals("track-10-09-2021_22h09-04s", route1.name)
+        assertTrue(route1.visible)
+        val markers1 = route1.routeMarkers
+        assertEquals(2, markers1.size.toLong())
+
+        val marker1 = markers1[0]
+        assertEquals("marker1", marker1.name)
+        val projX = marker1.proj_x
+        assertNotNull(projX)
+        assertEquals(6198798.5047565, projX, 0.0)
+
+        val marker2 = markers1[1]
+        assertEquals("marker2", marker2.name)
+        val marker2ProjY = marker2.proj_y
+        assertNotNull(marker2ProjY)
+        assertEquals(-2418744.7142449305, marker2ProjY, 0.0)
+
+        val route2 = map.routes!!.sortedBy { it.name }[1]
+        assertEquals("track-13-07-2021_10h27-44s", route2.name)
+        assertTrue(route2.visible)
+        val markers2 = route2.routeMarkers
+        assertEquals(2, markers2.size.toLong())
+
+        val marker3 = markers2[0]
+        assertEquals("marker3", marker3.name)
+        val marker3ProjY = marker3.proj_y
+        assertNotNull(marker3ProjY)
+        assertEquals(-2785744.45, marker3ProjY, 0.0)
+
+        val marker4 = markers2[1]
+        assertEquals("marker4", marker4.name)
+        val marker4ProjX = marker4.proj_x
+        assertNotNull(marker4ProjX)
+        assertEquals(5298798.45375, marker4ProjX, 0.0)
     }
 }
