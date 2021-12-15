c57918539a3394c94198d40931316612c78669f7
plr <plr.devs@gmail.com>
2021-12-15 17:53:47 +0100

    Update play-services-location

--- app/src/main/java/com/peterlaurence/trekme/core/repositories/location/producers/GoogleLocationProducer.kt
+++ app/src/main/java/com/peterlaurence/trekme/core/repositories/location/producers/GoogleLocationProducer.kt
@@ -44,8 +44,8 @@ class GoogleLocationProducer(private val applicationContext: Context) : Location
 
         return callbackFlow {
             val callback = object : LocationCallback() {
-                override fun onLocationResult(locationResult: LocationResult?) {
-                    for (loc in locationResult?.locations ?: listOf()) {
+                override fun onLocationResult(locationResult: LocationResult) {
+                    for (loc in locationResult.locations) {
                         val speed = if (loc.speed != 0f) loc.speed else null
                         val altitude = if (loc.altitude != 0.0) loc.altitude else null
                         trySend(Location(loc.latitude, loc.longitude, speed, altitude, loc.time, InternalGps))
