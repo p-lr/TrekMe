d98b756cc1074cf8f7af2c7f6a59504c40659fbb
plr <plr.devs@gmail.com>
2022-02-08 19:21:36 +0100

    Remove MapView - now using MapCompose exclusively

--- app/src/main/java/com/peterlaurence/trekme/viewmodel/common/tileviewcompat/CompatibityUtils.kt
+++ app/src/main/java/com/peterlaurence/trekme/viewmodel/common/tileviewcompat/CompatibityUtils.kt
@@ -1,36 +1,8 @@
 package com.peterlaurence.trekme.viewmodel.common.tileviewcompat
 
-import android.util.Log
-import com.peterlaurence.trekme.core.map.Map
 import com.peterlaurence.trekme.core.map.TileStream
 import com.peterlaurence.trekme.core.map.TileStreamProvider
-import java.io.File
-import java.io.FileInputStream
 import ovh.plrapps.mapcompose.core.TileStreamProvider as MapComposeTileStreamProvider
-import ovh.plrapps.mapview.core.TileStreamProvider as MapViewTileStreamProvider
-
-
-/**
- * This utility function converts the [Map]'s [TileStreamProvider] into whatever's type needed by
- * the view that fragments use to display tiles.
- * For instance, fragments use MapView, so the returned type is [MapViewTileStreamProvider].
- */
-fun makeMapViewTileStreamProvider(map: Map): MapViewTileStreamProvider? {
-    return if (map.origin != null) {
-        MapViewTileStreamProvider { row, col, zoomLvl ->
-            val relativePathString = "$zoomLvl${File.separator}$row${File.separator}$col${map.imageExtension}"
-
-            try {
-                FileInputStream(File(map.directory, relativePathString))
-            } catch (e: Exception) {
-                null
-            }
-        }
-    } else {
-        Log.e(TAG, "Unknown map origin ${map.origin}")
-        null
-    }
-}
 
 fun TileStreamProvider.toMapComposeTileStreamProvider(): MapComposeTileStreamProvider {
     return MapComposeTileStreamProvider { row, col, zoomLvl ->
