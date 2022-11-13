1e74a8e58c2a1ca60fd3bb49861c9e94dacc657c
plr <plr.devs@gmail.com>
2022-11-13 11:29:59 +0100

    Add geographic utility function

--- app/src/main/java/com/peterlaurence/trekme/core/geotools/GeoTools.kt
+++ app/src/main/java/com/peterlaurence/trekme/core/geotools/GeoTools.kt
@@ -1,5 +1,6 @@
 package com.peterlaurence.trekme.core.geotools
 
+import ovh.plrapps.mapcompose.utils.AngleDegree
 import kotlin.math.*
 
 /**
@@ -8,7 +9,8 @@ import kotlin.math.*
  * @author P.Laurence on 02/07/17.
  */
 
-private const val toRad = 0.017453292519943295 // 2*pi/360
+private const val toRad = 0.017453292519943295 // pi/180
+private const val toDeg = 57.29577951308232    // 180/pi
 
 /* WGS 84 ellipsoid parameters */
 private const val a: Double = 6_378_137.0       // radius at the equator, in meters
@@ -30,9 +32,9 @@ private const val radiusAvg: Double = 6_371_000.0  // radius average
  * @return the distance between the two points, in meters
  */
 fun distanceApprox(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
-    val a = Math.pow(Math.sin((lat2 - lat1) * toRad / 2), 2.0) + Math.cos(lat1 * toRad) * Math.cos(lat2 * toRad) *
-            Math.pow(Math.sin((lon2 - lon1) * toRad / 2), 2.0)
-    val c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+    val a = sin((lat2 - lat1) * toRad / 2).pow(2.0) + cos(lat1 * toRad) * cos(lat2 * toRad) *
+            sin((lon2 - lon1) * toRad / 2).pow(2.0)
+    val c = 2 * atan2(sqrt(a), sqrt(1 - a))
     return radiusAvg * c
 }
 
@@ -67,7 +69,9 @@ fun deltaTwoPoints(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Doub
  * @param ele2 the elevation of the second point, in meters
  * @return the distance between the two points, in meters
  */
-fun deltaTwoPoints(lat1: Double, lon1: Double, ele1: Double, lat2: Double, lon2: Double, ele2: Double): Double {
+fun deltaTwoPoints(
+    lat1: Double, lon1: Double, ele1: Double, lat2: Double, lon2: Double, ele2: Double
+): Double {
     val x = deltaTwoPoints(lat1, lon1, lat2, lon2)
     val y = abs(ele2 - ele1)
     return sqrt(x.pow(2) + y.pow(2))
@@ -83,7 +87,41 @@ fun deltaTwoPoints(lat1: Double, lon1: Double, ele1: Double, lat2: Double, lon2:
 fun earthRadius(lat: Double): Double {
     val aSq = a.pow(2)
     val bSq = b.pow(2)
-    val num = (aSq * cos(lat)).pow(2) + (bSq * sin(lat)).pow(2)
-    val den = aSq * cos(lat).pow(2) + bSq * sin(lat).pow(2)
+    val num = (aSq * cos(lat * toRad)).pow(2) + (bSq * sin(lat * toRad)).pow(2)
+    val den = aSq * cos(lat * toRad).pow(2) + bSq * sin(lat * toRad).pow(2)
     return sqrt(num / den)
 }
+
+/**
+ * Given a point, get the destination point at a given distance and bearing.
+ * Source: http://www.edwilliams.org/avform147.htm#LL
+ *
+ * @param lat1 the latitude of the original point, in decimal degrees
+ * @param lon1 the longitude of the original point, in decimal degrees
+ * @param distance the distance in meters from the original point
+ * @param angle the angle in decimal degrees between the course line and the local meridian measured
+ * clockwise. For example, 0° is North, 90° is East, 180° is South, 270° is West.
+ *
+ * @return a [DoubleArray] of size 2. First element is the latitude, the second is the longitude.
+ * Values are expressed in decimal degrees.
+ */
+fun pointAtDistanceAndAngle(
+    lat1: Double,
+    lon1: Double,
+    distance: Int,
+    angle: AngleDegree
+): DoubleArray {
+    val theta = distance / radiusAvg
+    val latRadians =
+        asin(sin(lat1 * toRad) * cos(theta) + cos(lat1 * toRad) * sin(theta) * cos(angle * toRad))
+    val dlon = atan2(
+        sin(angle * toRad) * sin(theta) * cos(lat1 * toRad),
+        cos(theta) - sin(lat1 * toRad) * sin(latRadians)
+    )
+
+    fun mod(y: Double, x: Double) = y - x * floor(y / x)
+    val lonRadians = PI - mod(-lon1 * toRad - dlon + PI, 2 * PI)
+
+    return doubleArrayOf(latRadians * toDeg, lonRadians * toDeg)
+}
+
--- app/src/test/java/com/peterlaurence/trekme/core/geotools/GeoToolsTest.kt
+++ app/src/test/java/com/peterlaurence/trekme/core/geotools/GeoToolsTest.kt
@@ -1,5 +1,6 @@
 package com.peterlaurence.trekme.core.geotools
 
+import junit.framework.TestCase.assertEquals
 import org.junit.Assert
 import org.junit.Test
 
@@ -12,5 +13,16 @@ class GeoToolsTest {
         val d = distanceApprox(51.510357, -0.116773, 38.889931, -77.009003)
         /* Check the result with 10cm precision */
         Assert.assertEquals(d, 5897658.289, 0.1)
+    }
+
+    @Test
+    fun pointAtDistanceAndAngleTest() {
+        val res1 = pointAtDistanceAndAngle(48.7221055, 2.5055106, 384260, 269.17f)
+        assertEquals(res1[0], 48.5536439, 0.00004)
+        assertEquals(res1[1], -2.7187781, 0.00004)
+
+        val res2 = pointAtDistanceAndAngle(16.218611, -61.745765, 413, 118.06f)
+        assertEquals(res2[0], 16.216855, 0.00001)
+        assertEquals(res2[1], -61.742333, 0.00002)
     }
 }
