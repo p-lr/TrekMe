3ae057be1d167292d9259feca4f549753a216c5e
plr <plr.devs@gmail.com>
2023-04-16 18:58:48 +0200

    Use POST for excursion search

--- app/src/main/java/com/peterlaurence/trekme/features/excursionsearch/data/api/ExcursionApiImpl.kt
+++ app/src/main/java/com/peterlaurence/trekme/features/excursionsearch/data/api/ExcursionApiImpl.kt
@@ -7,14 +7,18 @@ import com.peterlaurence.trekme.features.excursionsearch.domain.model.ExcursionA
 import com.peterlaurence.trekme.features.excursionsearch.presentation.model.ExcursionCategoryChoice
 import com.peterlaurence.trekme.util.performRequest
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 
 class ExcursionApiImpl(private val httpClient: OkHttpClient) : ExcursionApi {
     private val requestBuilder = Request.Builder()
     private val json = Json { isLenient = true; ignoreUnknownKeys = true }
     private val baseUrl = "http://192.168.1.18:8080/excursion"
+    private val mediaType = "application/json; charset=utf-8".toMediaType()
 
     override suspend fun search(
         lat: Double,
@@ -22,18 +26,23 @@ class ExcursionApiImpl(private val httpClient: OkHttpClient) : ExcursionApi {
         categoryChoice: ExcursionCategoryChoice
     ): List<ExcursionSearchItem> {
         val request = makeRequest(lat, lon, categoryChoice)
-        return httpClient.performRequest<List<Ref>>(request, json)?.map { it.toDomain() } ?: emptyList()
+        return httpClient.performRequest<List<Item>>(request, json)?.map { it.toDomain() } ?: emptyList()
     }
 
     private fun makeRequest(lat: Double, lon: Double, categoryChoice: ExcursionCategoryChoice): Request {
-        return when(categoryChoice) {
+        val form = when(categoryChoice) {
             ExcursionCategoryChoice.All -> {
-                requestBuilder.url("$baseUrl/search?lat=$lat&lon=$lon").build()
+                SearchForm(lat, lon)
             }
             is ExcursionCategoryChoice.Single -> {
-                requestBuilder.url("$baseUrl/search?lat=$lat&lon=$lon&category=${categoryChoice.choice.toApiString()}").build()
+                SearchForm(lat, lon, category = categoryChoice.choice.toApiString())
             }
         }
+
+        return requestBuilder
+            .url("$baseUrl/search")
+            .post(json.encodeToString(form).toRequestBody(mediaType))
+            .build()
     }
 
     private fun ExcursionCategory.toApiString(): String {
@@ -43,20 +52,23 @@ class ExcursionApiImpl(private val httpClient: OkHttpClient) : ExcursionApi {
             ExcursionCategory.Horse -> "horse"
             ExcursionCategory.Nautical -> "nautical"
             ExcursionCategory.Aerial -> "aerial"
-            ExcursionCategory.Motorised -> "motorized"
+            ExcursionCategory.Motorised -> "motorised"
         }
     }
 }
 
 @Serializable
-private data class Ref(
+private data class SearchForm(val lat: Double, val lon: Double, val radiusKm: Int? = null, val category: String? = null)
+
+@Serializable
+private data class Item(
     val id: String,
     val title: String,
     val type: String,
     val description: String = ""
 )
 
-private fun Ref.toDomain(): ExcursionSearchItem  {
+private fun Item.toDomain(): ExcursionSearchItem  {
     val typeDomain = when(type) {
         "hike" -> ExcursionType.Hike
         "running" -> ExcursionType.Running
