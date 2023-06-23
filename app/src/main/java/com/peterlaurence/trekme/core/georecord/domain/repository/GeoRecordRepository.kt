beafc0ac6776525a014b48911f3c5404e04161b3
plr <plr.devs@gmail.com>
2023-06-23 09:03:54 +0200

    Doc

--- app/src/main/java/com/peterlaurence/trekme/core/georecord/domain/repository/GeoRecordRepository.kt
+++ app/src/main/java/com/peterlaurence/trekme/core/georecord/domain/repository/GeoRecordRepository.kt
@@ -8,7 +8,6 @@ import com.peterlaurence.trekme.core.georecord.domain.model.GeoRecordLightWeight
 import com.peterlaurence.trekme.di.ApplicationScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -24,6 +23,13 @@ import javax.inject.Singleton
  * Since [GeoRecord]s are potentially heavy objects, this repository only exposes a [StateFlow] of
  * [GeoRecordLightWeight].
  * However, it's still possible to get a single [GeoRecord] using [getGeoRecord].
+ *
+ * Since excursions don't have a dedicated ui yet, they appear as regular recordings for the moment.
+ * Consequently, this repository supports two kinds of [GeoRecordLightWeight]:
+ * - the ones that refer to regular [GeoRecord]
+ * - the ones that refer to excursions
+ * Existing regular recordings will be eventually converted to excursions, so that excursions will
+ * be the only supported type.
  *
  * @since 2022/07/30
  */
