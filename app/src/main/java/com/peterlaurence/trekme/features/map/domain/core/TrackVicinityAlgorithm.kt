0b739772816dd1910eb4fdcfeb6200c9052fda4b
plr <plr.devs@gmail.com>
2023-09-02 12:25:49 +0200

    Avoid alerting on outlier location

--- app/src/main/java/com/peterlaurence/trekme/features/map/domain/core/TrackVicinityAlgorithm.kt
+++ app/src/main/java/com/peterlaurence/trekme/features/map/domain/core/TrackVicinityAlgorithm.kt
@@ -13,6 +13,7 @@ class TrackVicinityAlgorithm(private val trackVicinityVerifier: TrackVicinityVer
 
     private val timeSource = TimeSource.Monotonic
     private var lastAlertTime: TimeSource.Monotonic.ValueTimeMark? = null
+    private var outsideCount = 0
 
     /**
      * If the location isn't in the vicinity, fire an alert if the last alert occurred more than 20
@@ -21,15 +22,23 @@ class TrackVicinityAlgorithm(private val trackVicinityVerifier: TrackVicinityVer
     suspend fun processLocation(location: Location, threshold: Int): Boolean {
         val isInside = trackVicinityVerifier.isInVicinity(location.latitude, location.longitude, threshold)
         return if (!isInside) {
-            val mark = timeSource.markNow()
-            lastAlertTime?.let { lastMark ->
-                val shouldAlert = (mark - lastMark).inWholeSeconds > 20
-                if (shouldAlert) lastAlertTime = mark
-                shouldAlert
-            } ?: run {
-                lastAlertTime = mark
-                true
-            }
-        } else false
+            outsideCount++
+
+            /* Alert only if we have at least two consecutive outside locations, to avoid alerting on an outlier location. */
+            if (outsideCount > 1) {
+                val mark = timeSource.markNow()
+                lastAlertTime?.let { lastMark ->
+                    val shouldAlert = (mark - lastMark).inWholeSeconds > 20
+                    if (shouldAlert) lastAlertTime = mark
+                    shouldAlert
+                } ?: run {
+                    lastAlertTime = mark
+                    true
+                }
+            } else false
+        } else {
+            outsideCount = 0
+            false
+        }
     }
 }
