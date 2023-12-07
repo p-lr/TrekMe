f827ca36f763f1fb454856f6544bc28b42f50bea
Ivan Yakushev <theycome@mail.ru>
2023-12-07 21:13:36 +0300

    Refactor RouteMapper

--- app/src/main/java/com/peterlaurence/trekme/core/map/data/mappers/RouteMapper.kt
+++ app/src/main/java/com/peterlaurence/trekme/core/map/data/mappers/RouteMapper.kt
@@ -5,19 +5,17 @@ import com.peterlaurence.trekme.core.map.data.models.RouteInfoKtx
 import com.peterlaurence.trekme.core.map.data.models.RouteKtx
 
 fun Route.toRouteKtx(): RouteKtx {
-    val r = this
     return RouteKtx(
-        markers = r.routeMarkers.map { it.toMarkerKtx() }
+        markers = routeMarkers.map { it.toMarkerKtx() }
     )
 }
 
 fun Route.toRouteInfoKtx(): RouteInfoKtx {
-    val r = this
     return RouteInfoKtx(
-        id = r.id,
-        name = r.name.value,
-        color = r.color.value,
-        visible = r.visible.value,
-        elevationTrusted = r.elevationTrusted,
+        id = id,
+        name = name.value,
+        color = color.value,
+        visible = visible.value,
+        elevationTrusted = elevationTrusted,
     )
 }
