cebb26f21fa11840cf8c109e4baf0847eb5adb4f
plr <plr.devs@gmail.com>
2024-03-10 11:54:35 +0100

    Use material3 top app bar in gps pro

--- app/src/main/java/com/peterlaurence/trekme/features/gpspro/presentation/ui/screens/GpsProFragment.kt
+++ app/src/main/java/com/peterlaurence/trekme/features/gpspro/presentation/ui/screens/GpsProFragment.kt
@@ -1,35 +1,33 @@
 package com.peterlaurence.trekme.features.gpspro.presentation.ui.screens
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.core.view.MenuHost
-import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Lifecycle
-import com.peterlaurence.trekme.R
+import com.peterlaurence.trekme.events.AppEventBus
 import com.peterlaurence.trekme.features.common.presentation.ui.theme.TrekMeTheme
-import com.peterlaurence.trekme.features.gpspro.presentation.viewmodel.GpsProViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class GpsProFragment : Fragment() {
-    val viewModel: GpsProViewModel by activityViewModels()
+
+    @Inject
+    lateinit var appEventBus: AppEventBus
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        /* The action bar isn't managed by Compose */
+        /* The action bar is managed by Compose */
         (requireActivity() as AppCompatActivity).supportActionBar?.apply {
-            show()
-            title = getString(R.string.select_bt_devices_title)
+            hide()
+            title = ""
         }
 
         return ComposeView(requireContext()).apply {
@@ -37,44 +35,11 @@ class GpsProFragment : Fragment() {
 
             setContent {
                 TrekMeTheme {
-                    GpsProUI(
-                        bluetoothState = viewModel.bluetoothState,
-                        isHostSelected = viewModel.isHostSelected,
-                        onHostSelection = viewModel::onHostSelected,
-                        onBtDeviceSelection = viewModel::onBtDeviceSelection,
-                        onShowSettings = viewModel::onShowBtDeviceSettings
+                    GpsProStateful(
+                        onMainMenuClick = appEventBus::openDrawer
                     )
                 }
             }
         }
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setupMenu()
-    }
-
-    private fun setupMenu() {
-        (requireActivity() as MenuHost).addMenuProvider(object : MenuProvider {
-            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                /* Clear the existing action menu */
-                menu.clear()
-
-                /* Fill the new one */
-                menuInflater.inflate(R.menu.menu_fragment_gpspro, menu)
-            }
-
-            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-                return when (menuItem.itemId) {
-                    R.id.help_gpspro_id -> {
-                        val url = getString(R.string.gps_pro_help_url)
-                        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-                        startActivity(browserIntent)
-                        true
-                    }
-                    else -> false
-                }
-            }
-        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 }
--- app/src/main/java/com/peterlaurence/trekme/features/gpspro/presentation/ui/screens/GpsProUI.kt
+++ app/src/main/java/com/peterlaurence/trekme/features/gpspro/presentation/ui/screens/GpsProUI.kt
@@ -1,39 +1,120 @@
 package com.peterlaurence.trekme.features.gpspro.presentation.ui.screens
 
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.peterlaurence.trekme.R
 import com.peterlaurence.trekme.features.common.presentation.ui.screens.ErrorScreen
 import com.peterlaurence.trekme.features.common.presentation.ui.theme.accentGreen
 import com.peterlaurence.trekme.features.common.presentation.ui.theme.dark_accentGreen
 import com.peterlaurence.trekme.features.gpspro.presentation.ui.components.IconCircle
-import com.peterlaurence.trekme.features.gpspro.presentation.viewmodel.*
-
+import com.peterlaurence.trekme.features.gpspro.presentation.viewmodel.BluetoothDeviceStub
+import com.peterlaurence.trekme.features.gpspro.presentation.viewmodel.BluetoothState
+import com.peterlaurence.trekme.features.gpspro.presentation.viewmodel.BtConnectPermNotGranted
+import com.peterlaurence.trekme.features.gpspro.presentation.viewmodel.BtDisabled
+import com.peterlaurence.trekme.features.gpspro.presentation.viewmodel.BtNotSupported
+import com.peterlaurence.trekme.features.gpspro.presentation.viewmodel.GpsProViewModel
+import com.peterlaurence.trekme.features.gpspro.presentation.viewmodel.PairedDeviceList
+import com.peterlaurence.trekme.features.gpspro.presentation.viewmodel.Searching
+import com.peterlaurence.trekme.util.android.activity
 
 @Composable
-fun GpsProUI(
+fun GpsProStateful(
+    viewModel: GpsProViewModel = hiltViewModel(viewModelStoreOwner = LocalContext.current.activity),
+    onMainMenuClick: () -> Unit
+) {
+    val uriHandler = LocalUriHandler.current
+    val helpUri = stringResource(id = R.string.gps_pro_help_url)
+
+    GpsProUI(
+        bluetoothState = viewModel.bluetoothState,
+        isHostSelected = viewModel.isHostSelected,
+        onHostSelection = viewModel::onHostSelected,
+        onBtDeviceSelection = viewModel::onBtDeviceSelection,
+        onShowSettings = viewModel::onShowBtDeviceSettings,
+        onMainMenuClick = onMainMenuClick,
+        onShowHelp = {
+            uriHandler.openUri(helpUri)
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun GpsProUI(
     bluetoothState: BluetoothState,
     isHostSelected: Boolean,
     onHostSelection: () -> Unit,
     onBtDeviceSelection: (BluetoothDeviceStub) -> Unit,
-    onShowSettings: () -> Unit
+    onShowSettings: () -> Unit,
+    onMainMenuClick: () -> Unit,
+    onShowHelp: () -> Unit
 ) {
-    Surface {
-        Column {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.wifip2p_title)) },
+                navigationIcon = {
+                    IconButton(onClick = onMainMenuClick) {
+                        Icon(Icons.Filled.Menu, contentDescription = "")
+                    }
+                },
+                actions = {
+                    IconButton(
+                        onClick = onShowHelp,
+                        modifier = Modifier.width(36.dp)
+                    ) {
+                        Icon(
+                            painterResource(id = R.drawable.help_circle_outline_white),
+                            contentDescription = null,
+                        )
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
+        Column(Modifier.padding(paddingValues)) {
             HostDeviceLine(
                 name = stringResource(id = R.string.internal_gps),
                 isHostSelected,
@@ -70,6 +151,7 @@ fun BluetoothUI(
                 }
             }
         }
+
         BtDisabled -> ErrorScreen(message = stringResource(id = R.string.gps_pro_bt_disabled))
         BtNotSupported -> ErrorScreen(message = stringResource(id = R.string.gps_pro_bt_notsupported))
         BtConnectPermNotGranted -> ErrorScreen(message = stringResource(id = R.string.gps_pro_bt_perm_not_granted))
