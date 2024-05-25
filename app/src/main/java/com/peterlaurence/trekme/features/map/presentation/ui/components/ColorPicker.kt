8441b13d3fe60f1a860b9b6f1b2eff9c02854d21
plr <plr.devs@gmail.com>
2024-05-25 10:19:34 +0200

    Add colors to ColorPicker

--- app/src/main/java/com/peterlaurence/trekme/features/map/presentation/ui/components/ColorPicker.kt
+++ app/src/main/java/com/peterlaurence/trekme/features/map/presentation/ui/components/ColorPicker.kt
@@ -4,7 +4,9 @@ import android.content.res.Configuration
 import android.os.Parcelable
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -16,7 +18,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.peterlaurence.trekme.R
 import com.peterlaurence.trekme.features.common.presentation.ui.theme.TrekMeTheme
 import kotlinx.parcelize.Parcelize
@@ -41,75 +42,47 @@ fun ColorPicker(
         }
     }
     var activeIndex by remember { mutableIntStateOf(initValues?.second ?: 0) }
+    val scrollState = rememberScrollState()
 
     AlertDialog(
         onDismissRequest = onCancel,
+        title = {
+            Text(
+                stringResource(id = R.string.color_picker_title)
+            )
+        },
         text = {
             Column(
-                Modifier.padding(top = 16.dp)
+                Modifier
+                    .verticalScroll(scrollState)
             ) {
-                Text(
-                    stringResource(id = R.string.color_picker_title),
-                    modifier = Modifier.padding(start = 16.dp, bottom = 24.dp),
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Medium
-                )
                 val strokeColor = MaterialTheme.colorScheme.onSurface
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(36.dp),
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    repeat(3) { index ->
-                        Surface(
-                            onClick = { activeIndex = index },
-                            shape = CircleShape,
-                            color = Color(palette[index]),
-                            modifier = Modifier
-                                .padding(horizontal = if (index == activeIndex) 0.dp else 3.dp)
-                                .size(if (index == activeIndex) 36.dp else 30.dp)
-                                .drawBehind {
-                                    if (index == activeIndex) {
-                                        drawCircle(
-                                            strokeColor,
-                                            19.dp.toPx(),
-                                            style = Stroke(3.dp.toPx())
-                                        )
-                                    }
-                                }
-                        ) {}
-                    }
-                }
+                Spacer(modifier = Modifier.height(8.dp))
+                ColorRow(
+                    activeIndex,
+                    palette,
+                    offset = 0,
+                    strokeColor = strokeColor,
+                    onClick = { activeIndex = it }
+                )
+
                 Spacer(modifier = Modifier.height(16.dp))
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(36.dp),
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    repeat(3) { index ->
-                        Surface(
-                            onClick = { activeIndex = 3 + index },
-                            shape = CircleShape,
-                            color = Color(palette[3 + index]),
-                            modifier = Modifier
-                                .padding(horizontal = if (3 + index == activeIndex) 0.dp else 3.dp)
-                                .size(if (3 + index == activeIndex) 36.dp else 30.dp)
-                                .drawBehind {
-                                    if (3 + index == activeIndex) {
-                                        drawCircle(
-                                            strokeColor,
-                                            19.dp.toPx(),
-                                            style = Stroke(3.dp.toPx())
-                                        )
-                                    }
-                                }
-                        ) {}
-                    }
-                }
+                ColorRow(
+                    activeIndex,
+                    palette,
+                    offset = 3,
+                    strokeColor = strokeColor,
+                    onClick = { activeIndex = it }
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+                ColorRow(
+                    activeIndex,
+                    palette,
+                    offset = 6,
+                    strokeColor,
+                    onClick = { activeIndex = it }
+                )
 
                 Row(Modifier.padding(start = 16.dp, top = 16.dp)) {
                     Text(
@@ -178,6 +151,43 @@ fun ColorPicker(
     )
 }
 
+@Composable
+private fun ColorRow(
+    activeIndex: Int,
+    palette: List<Long>,
+    offset: Int,
+    strokeColor: Color,
+    onClick: (index: Int) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(36.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        repeat(3) { index ->
+            Surface(
+                onClick = { onClick(offset + index) },
+                shape = CircleShape,
+                color = Color(palette[offset + index]),
+                modifier = Modifier
+                    .padding(horizontal = if (offset + index == activeIndex) 0.dp else 3.dp)
+                    .size(if (offset + index == activeIndex) 36.dp else 30.dp)
+                    .drawBehind {
+                        if (offset + index == activeIndex) {
+                            drawCircle(
+                                strokeColor,
+                                19.dp.toPx(),
+                                style = Stroke(3.dp.toPx())
+                            )
+                        }
+                    }
+            ) {}
+        }
+    }
+}
+
 private fun findColorInPalettes(color: Long): Pair<PaletteVariant, Int>? {
     PaletteVariant.entries.forEach { variant ->
         val palette = when (variant) {
@@ -194,15 +204,18 @@ private fun findColorInPalettes(color: Long): Pair<PaletteVariant, Int>? {
 }
 
 private val normalPalette = listOf(
-    0xfff44336, 0xff9c27b0, 0xff2196f3, 0xff4caf50, 0xff755548, 0xff607d8b
+    0xfff44336, 0xff9c27b0, 0xff2196f3, 0xff4caf50, 0xff755548, 0xff607d8b,
+    0xffFF9800, 0xffffeb3b, 0xff3F51B5
 )
 
 private val lightPalette = listOf(
-    0xffff7961, 0xffd05ce3, 0xff6ec6ff, 0xff80e27e, 0xffa98274, 0xff8eacbb
+    0xffff7961, 0xffd05ce3, 0xff6ec6ff, 0xff80e27e, 0xffa98274, 0xff8eacbb,
+    0xffffb84d, 0xfffef075, 0xff7986cb
 )
 
 private val darkPalette = listOf(
-    0xffba000d, 0xff6a0080, 0xff0069c0, 0xff087f23, 0xff4b2c20, 0xff34515e
+    0xffba000d, 0xff6a0080, 0xff0069c0, 0xff087f23, 0xff4b2c20, 0xff34515e,
+    0xffff7d00, 0xfffbc02d, 0xff303f9f
 )
 
 @Parcelize
