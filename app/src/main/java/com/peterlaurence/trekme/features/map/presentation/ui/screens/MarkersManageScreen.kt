bbb5c21844c5f0344abb5c01ddbc6581e6916421
plr <plr.devs@gmail.com>
2024-05-28 07:49:32 +0200

    Markers manage ui (wip)

--- app/src/main/java/com/peterlaurence/trekme/features/map/presentation/ui/screens/MarkersManageScreen.kt
+++ app/src/main/java/com/peterlaurence/trekme/features/map/presentation/ui/screens/MarkersManageScreen.kt
@@ -1,16 +1,31 @@
 package com.peterlaurence.trekme.features.map.presentation.ui.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Icon
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -18,19 +33,26 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.peterlaurence.trekme.R
 import com.peterlaurence.trekme.core.map.domain.models.Marker
+import com.peterlaurence.trekme.features.common.presentation.ui.theme.TrekMeTheme
+import com.peterlaurence.trekme.features.map.presentation.ui.components.Marker
 import com.peterlaurence.trekme.features.map.presentation.viewmodel.MarkersManageViewModel
+import com.peterlaurence.trekme.util.darkenColor
+import com.peterlaurence.trekme.util.parseColor
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
 
 @Composable
 fun MarkersManageStateful(
@@ -64,6 +86,7 @@ fun MarkersManageStateful(
     )
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun MarkersManageScreen(
     markers: List<Marker>,
@@ -78,7 +101,9 @@ private fun MarkersManageScreen(
     ) { paddingValues ->
         if (hasMarkers) {
             var searchText by remember { mutableStateOf("") }
-            Column {
+            Column(
+                Modifier.padding(paddingValues)
+            ) {
                 OutlinedTextField(
                     value = searchText,
                     onValueChange = {
@@ -90,12 +115,24 @@ private fun MarkersManageScreen(
                             painter = painterResource(id = R.drawable.ic_baseline_search_24),
                             contentDescription = stringResource(id = R.string.search_hint)
                         )
-                    }
+                    },
+                    placeholder = {
+                        Text(text = stringResource(id = R.string.search_hint))
+                    },
+                    shape = RoundedCornerShape(50),
+                    modifier = Modifier.padding(horizontal = 8.dp)
+//                    modifier = Modifier.border(1.dp, Color.DarkGray, shape = RoundedCornerShape(50))
                 )
 
-                LazyColumn {
+                LazyColumn(
+                    contentPadding = PaddingValues(8.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                ) {
                     items(markers, key = { it.id }) {
-                        Text(text = it.name)
+                        MarkerCard(
+                            modifier = Modifier.animateItemPlacement(),
+                            marker = it
+                        )
                     }
                 }
             }
@@ -112,7 +149,7 @@ private fun MarkersManageScreen(
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun MarkersTopAppBar(onBackClick: () -> Unit,) {
+private fun MarkersTopAppBar(onBackClick: () -> Unit) {
     TopAppBar(
         title = { Text(text = stringResource(id = R.string.markers_manage_title)) },
         navigationIcon = {
@@ -122,4 +159,72 @@ private fun MarkersTopAppBar(onBackClick: () -> Unit,) {
         },
         actions = { }
     )
+}
+
+@Composable
+private fun MarkerCard(
+    modifier: Modifier = Modifier,
+    marker: Marker
+) {
+    ElevatedCard(modifier = modifier) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            val backgroundColor = parseColor(marker.color)
+            val strokeColor = darkenColor(backgroundColor, 0.15f)
+
+            Marker(
+                modifier = Modifier
+                    .graphicsLayer {
+                        clip = true
+                        translationY = 10.dp.toPx()
+                    }
+                    .padding(horizontal = 8.dp),
+                backgroundColor = Color(backgroundColor),
+                strokeColor = Color(strokeColor),
+                isStatic = true
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            if (marker.name.isNotEmpty()) {
+                Text(text = marker.name)
+            } else {
+                Text(
+                    text = stringResource(id = R.string.markers_manage_no_name),
+                    fontStyle = FontStyle.Italic,
+                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+                )
+            }
+            Spacer(modifier = Modifier.weight(1f))
+
+            VerticalDivider(Modifier.height(24.dp))
+            Icon(
+                Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                modifier = Modifier.padding(horizontal = 8.dp),
+                contentDescription = stringResource(id = R.string.open_dialog)
+            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+private fun MarkersManagePreview() {
+    TrekMeTheme {
+        val markers = buildList<Marker> {
+            repeat(6) {
+                add(Marker(lat = 12.6, lon = 2.57, name = "marker-$it"))
+                add(Marker(lat = 12.6, lon = 2.57))
+            }
+        }
+
+        MarkersManageScreen(
+            markers = markers,
+            hasMarkers = true,
+            onNewSearch = {},
+            onBackClick = {}
+        )
+    }
 }
--- app/src/test/java/com/peterlaurence/trekme/core/map/MapImportInteractorTest.kt
+++ app/src/test/java/com/peterlaurence/trekme/core/map/MapImportInteractorTest.kt
@@ -60,13 +60,13 @@ class MapImportInteractorTest {
                          * extracted from an archive, we don't know whether the map was zipped within a
                          * subdirectory or not. A way to know that is to analyse the extracted file structure.
                          */
-                        assertEquals("mapname", map.name)
+                        assertEquals("mapname", map.name.value)
 
-                        assertEquals(4, map.configSnapshot.levels.size.toLong())
-                        assertEquals(256, map.configSnapshot.levels[0].tileSize.width.toLong())
+                        assertEquals(4, map.levelList.size.toLong())
+                        assertEquals(256, map.levelList[0].tileSize.width.toLong())
                         assertEquals(".jpg", map.imageExtension)
                         assertEquals(true, File(expectedParentFolder, ".nomedia").exists())
-                        assertNull(map.thumbnailImage)
+                        assertNull(map.thumbnail.value)
                     } catch (e: Exception) {
                         fail()
                     }
@@ -84,8 +84,8 @@ class MapImportInteractorTest {
                     try {
                         val res = mapImportInteractor.importFromFile(libVipsMapDir)
                         val map = assertNotNull(res.map)
-                        assertEquals("La Réunion - Est", map.name)
-                        assertEquals(3, map.configSnapshot.levels.size.toLong())
+                        assertEquals("La Réunion - Est", map.name.value)
+                        assertEquals(3, map.levelList.size.toLong())
                         val expectedParentFolder = File(libVipsMapDir, "reunion-est")
                         assertEquals(true, File(expectedParentFolder, ".nomedia").exists())
                     } catch (e: Exception) {
